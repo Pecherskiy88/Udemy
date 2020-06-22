876d454d7930896c85bc31da194e5e10ed57e14d
Pecherskiy88 <pecherskiy88@gmail.com>
2020-06-22 21:55:36 +0300

    did action when ball tuch world boards

--- GameDev/Arkanoid/game.js
+++ GameDev/Arkanoid/game.js
@@ -71,10 +71,11 @@ let game = {
   },
 
   update() {
-    this.platform.move();
-    this.ball.move();
     this.collideBlocks();
     this.collidePlatform();
+    this.ball.collideWorldBounds();
+    this.platform.move();
+    this.ball.move();
   },
 
   collideBlocks() {
@@ -173,14 +174,43 @@ game.ball = {
     }
     return false;
   },
+  collideWorldBounds() {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+
+    let ballLeft = x;
+    let ballRight = ballLeft + this.width;
+    let ballTop = y;
+    let ballBottom = ballTop + this.height;
+
+    let worldLeft = 0;
+    let worldRight = game.width;
+    let worldTop = 0;
+    let worldBottom = game.height;
+
+    if (ballLeft < worldLeft) {
+      this.x = 0;
+      this.dx = this.velocity;
+    } else if (ballRight > worldRight) {
+      this.x = worldRight - this.width;
+      this.dx = -this.velocity;
+    } else if (ballTop < worldTop) {
+      this.y = 0;
+      this.dy = this.velocity;
+    } else if (ballBottom > worldBottom) {
+      console.log('GAME OVER!');
+    }
+  },
   bumbBlock(block) {
     this.dy = -this.dy;
     block.active = false;
   },
   bumpPlatform(platform) {
-    this.dy = -this.dy;
-    let touchX = this.x + this.width / 2;
-    this.dx = this.velocity * platform.getTouchOffset(touchX);
+    if (this.dy > 0) {
+      this.dy = -this.velocity;
+      let touchX = this.x + this.width / 2;
+      this.dx = this.velocity * platform.getTouchOffset(touchX);
+    }
   },
 };
 
