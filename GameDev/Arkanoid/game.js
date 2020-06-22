5843b48d1097d7b3bda51d9d56ae50fb4af90601
Pecherskiy88 <pecherskiy88@gmail.com>
2020-06-22 20:14:17 +0300

    did break blocks functionality

--- GameDev/Arkanoid/game.js
+++ GameDev/Arkanoid/game.js
@@ -1,6 +1,7 @@
 const KEYS = {
   LEFT: 37,
   RIGHT: 39,
+  SPACE: 32,
 };
 
 let game = {
@@ -10,6 +11,8 @@ let game = {
   blocks: [],
   rows: 4,
   cols: 8,
+  width: 640,
+  height: 360,
   sprites: {
     background: null,
     ball: null,
@@ -24,7 +27,9 @@ let game = {
 
   setEvents() {
     window.addEventListener('keydown', (e) => {
-      if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+      if (e.keyCode === KEYS.SPACE) {
+        this.platform.fire();
+      } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
         this.platform.start(e.keyCode);
       }
     });
@@ -55,6 +60,9 @@ let game = {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          width: 60,
+          height: 20,
+          active: true,
           x: 64 * col + 65,
           y: 24 * row + 35,
         });
@@ -64,6 +72,23 @@ let game = {
 
   update() {
     this.platform.move();
+    this.ball.move();
+    this.collideBlocks();
+    this.collidePlatform();
+  },
+
+  collideBlocks() {
+    for (let block of this.blocks) {
+      if (block.active && this.ball.collide(block)) {
+        this.ball.bumbBlock(block);
+      }
+    }
+  },
+
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
+    }
   },
 
   run() {
@@ -75,6 +100,7 @@ let game = {
   },
 
   render() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.drawImage(this.sprites.background, 0, 0);
     this.ctx.drawImage(
       this.sprites.ball,
@@ -93,7 +119,9 @@ let game = {
 
   renderBlocks() {
     for (let block of this.blocks) {
-      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      if (block.active) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }
     }
   },
 
@@ -104,13 +132,56 @@ let game = {
       this.run();
     });
   },
+
+  random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 };
 
 game.ball = {
+  dy: 0,
+  dx: 0,
+  velocity: 3,
   x: 320,
   y: 280,
   width: 20,
   height: 20,
+  start() {
+    this.dy = -this.velocity;
+    this.dx = game.random(-this.velocity, this.velocity);
+  },
+  move() {
+    if (this.dy) {
+      this.y += this.dy;
+    }
+    if (this.dx) {
+      this.x += this.dx;
+    }
+  },
+  collide(element) {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+
+    // попадание по блоку определеяется 4мя условиями:
+    if (
+      x + this.width > element.x && // мяч наползает на блок слева
+      x < element.x + element.width && // мяч наползает на блок справа
+      y + this.height > element.y && // мяч наползает на блок сверху
+      y < element.y + element.height // мяч наползает сниху
+    ) {
+      return true;
+    }
+    return false;
+  },
+  bumbBlock(block) {
+    this.dy = -this.dy;
+    block.active = false;
+  },
+  bumpPlatform(platform) {
+    this.dy = -this.dy;
+    let touchX = this.x + this.width / 2;
+    this.dx = this.velocity * platform.getTouchOffset(touchX);
+  },
 };
 
 game.platform = {
@@ -118,6 +189,15 @@ game.platform = {
   dx: 0,
   x: 280,
   y: 300,
+  width: 100,
+  height: 14,
+  ball: game.ball,
+  fire() {
+    if (this.ball) {
+      this.ball.start();
+      this.ball = null;
+    }
+  },
   start(direction) {
     if (direction === KEYS.LEFT) {
       this.dx = -this.velocity;
@@ -131,8 +211,21 @@ game.platform = {
   move() {
     if (this.dx) {
       this.x += this.dx;
-      game.ball.x += this.dx;
+
+      if (this.ball) {
+        this.ball.x += this.dx;
+      }
     }
+  },
+
+  getTouchOffset(x) {
+    let diff = this.x + this.width - x;
+    let offset = this.width - diff;
+    // this.width = 2; Пропорция
+    // offset = ? (result)
+    let result = (offset * 2) / this.width;
+
+    return result - 1;
   },
 };
 
